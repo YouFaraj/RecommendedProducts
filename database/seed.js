9a99d99eb36c4b640767c7c818556b4be8eb0ebe
Yousif Faraj <yfaraj96@gmail.com>
2020-10-24 19:46:34 -0400

    rewrote seed to use streams

--- database/seed.js
+++ database/seed.js
@@ -1,15 +1,12 @@
 const faker = require('faker');
 const fs = require('fs');
+const file = fs.createWriteStream('./data/products_data.csv');
+file.write('id,title,brand,dept,price,imageurl,producturl' + '\n');
 
-var id = -1;
 var brands = [];
 var departments = [];
+var data = '';
 
-// generate ids
-const idGenerator = function() {
-  id = id + 1;
-  return id;
-}
 
 // generates depts and brands
 const brandsAndDepts = function() {
@@ -24,42 +21,50 @@ const brandsAndDepts = function() {
 const generateRecords = function(num) {
 var records = ``
   for(let i = 0; i < num; i++){
-      records += `
-          ${idGenerator()},"${faker.commerce.productName()}","${brands[ Math.floor( Math.random() * brands.length ) ]}","${departments[ Math.floor( Math.random() * departments.length ) ]}",${Number(faker.commerce.price(0, 100)) - Math.ceil(Math.random() * 5) / 100},"${faker.image.imageUrl()}","/${Math.floor(Math.random() * 100 % 100 + 1)}"`
+
       }
   return records;
 };
 
-// creates a file if none exists and appends to it the records
-const writeRecords = function(num){
-  const records = generateRecords(num);
-  fs.appendFileSync('./data/products_data.csv', records, (err) => {
-      if (err) {
-        console.log('error:', err);
-      } else {
-        console.log('example data file has been written');
+
+function writeTwentyMillionTimes(writer) {
+  let i = 20000;
+  let chunk = 1000;
+  let max = i;
+  write();
+  function write() {
+    let ok = true;
+    console.log(`Function call with ${i} of ${max}`);
+    do {
+      i--;
+
+      ///// data gen /////
+      id = max - i;
+      data += `${id},"${faker.commerce.productName()}","${brands[ Math.floor( Math.random() * brands.length ) ]}","${departments[ Math.floor( Math.random() * departments.length ) ]}",${Number(faker.commerce.price(0, 100)) - Math.ceil(Math.random() * 5) / 100},"${faker.image.imageUrl()}","/${Math.floor(Math.random() * 100 % 100 + 1)}"\n`
+      ////////////////////
+      if (i === 0) {
+        // Last time!
+        writer.write(data);
+      } else if (i % chunk === 0) {
+        // See if we should continue, or wait.
+        // Don't pass the callback, because we're not done yet.
+        console.log(`${i} of ${max}`);
+        ok = writer.write(data);
+        data = '';
       }
-  });
-}
-
-
-const seed = function() {
-  brandsAndDepts()
-      fs.appendFileSync('./data/products_data.csv', 'id,title,brand,department,price,imageurl,producturl', (err) => {
-        if (err) {
-          console.log('error:', err);
-        } else {
-          console.log('example data file has been written');
-        }
-    });
-
-  for(let i = 0; i < 20000; i++){
-    writeRecords(1000)
-    console.log(`${id} of 20 million done`)
+    } while (i >= 0 && ok);
+    if (i >= 0) {
+      // Had to stop early!
+      // Write some more once it drains.
+      console.log('########## waiting for the drain event ############')
+      writer.once('drain', write);
+    }
   }
 }
 
-seed()
+console.time('load');
+writeTwentyMillionTimes(file);
+console.timeEnd('load')
 
 
 
